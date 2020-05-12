Min-BC & weighting fix

mtil/algos/mtbc/__main__.py
@@ -1,5 +1,6 @@
 import contextlib
 import multiprocessing as mp
+import itertools as it
 import os
 import readline  # noqa: F401
 import time
@@ -14,7 +15,8 @@ from rlpyt.samplers.parallel.gpu.sampler import GpuSampler
 from rlpyt.utils.logging import logger
 import torch
 
-from mtil.algos.mtbc.mtbc import (copy_model_into_agent_eval,
+from mtil.algos.mtbc.mtbc import (MinBCWeightingModule,
+                                  copy_model_into_agent_eval,
                                   do_epoch_training_mt, eval_model,
                                   eval_model_st, get_latest_path, make_env_tag,
                                   saved_model_loader_ft, strip_mb_preproc_name,
@@ -90,11 +92,15 @@ def cli():
 @click.option("--snapshot-gap",
               default=10,
               help="how many evals to wait for before saving snapshot")
+@click.option("--min-bc/--no-min-bc",
+              default=False,
+              help="use the 'min BC' objective, where the policy is only "
+              "required to mimic the 'easiest' trajectory for each task")
 @click.argument("demos", nargs=-1, required=True)
 def train(demos, add_preproc, seed, batch_size, epochs, out_dir, run_name,
           gpu_idx, eval_n_traj, passes_per_eval, snapshot_gap, omit_noop,
           net_width_mul, net_use_bn, net_dropout, net_coord_conv,
-          net_attention, aug_mode):
+          net_attention, aug_mode, min_bc):
     # TODO: abstract setup code. Seeds & GPUs should go in one function. Env
     # setup should go in another function (or maybe the same function). Dataset
     # loading should be simplified by having a single class that can provide
@@ -135,6 +141,7 @@ def train(demos, add_preproc, seed, batch_size, epochs, out_dir, run_name,
         loader_mt = make_loader_mt(dataset_mt, batch_size)
         variant_groups = demos_metas_dict['variant_groups']
         env_metas = demos_metas_dict['env_metas']
+        num_demo_sources = demos_metas_dict['num_demo_sources']
         task_ids_and_demo_env_names = demos_metas_dict[
             'task_ids_and_demo_env_names']
         sampler_batch_B = batch_size
@@ -142,6 +149,7 @@ def train(demos, add_preproc, seed, batch_size, epochs, out_dir, run_name,
         sampler_batch_T = 5
         sampler, sampler_batch_B = make_mux_sampler(
             variant_groups=variant_groups,
+            num_demo_sources=num_demo_sources,
             env_metas=env_metas,
             use_gpu=use_gpu,
             batch_B=sampler_batch_B,
@@ -154,12 +162,21 @@ def train(demos, add_preproc, seed, batch_size, epochs, out_dir, run_name,
         exit_stack.callback(lambda: sampler.shutdown())
 
         model_mt = policy_ctor(**policy_kwargs).to(dev)
+        if min_bc:
+            num_tasks = len(task_ids_and_demo_env_names)
+            weight_mod = MinBCWeightingModule(num_tasks, num_demo_sources) \
+                .to(dev)
+            all_params = it.chain(model_mt.parameters(),
+                                  weight_mod.parameters())
+        else:
+            weight_mod = None
+            all_params = model_mt.parameters()
         # Adam mostly works fine, but in very loose informal tests it seems
         # like SGD had fewer weird failures where mean loss would jump up by a
         # factor of 2x for a period (?). (I don't think that was solely due to
         # high LR; probably an architectural issue.) opt_mt =
         # torch.optim.Adam(model_mt.parameters(), lr=3e-4)
-        opt_mt = torch.optim.SGD(model_mt.parameters(), lr=1e-3, momentum=0.1)
+        opt_mt = torch.optim.SGD(all_params, lr=1e-3, momentum=0.1)
 
         try:
             aug_opts = MILBenchAugmentations.PRESETS[aug_mode]
@@ -213,7 +230,7 @@ def train(demos, add_preproc, seed, batch_size, epochs, out_dir, run_name,
                 model_mt.train()
                 loss_ewma, losses, per_task_losses = do_epoch_training_mt(
                     loader_mt, model_mt, opt_mt, dev, passes_per_eval,
-                    aug_model)
+                    aug_model, weight_mod)
 
                 # TODO: record accuracy on a random subset of the train and
                 # validation sets (both in eval mode, not train mode)

mtil/demos.py
@@ -182,16 +182,18 @@ def add_mb_preproc(demo_trajs_by_env, variant_names, mb_preproc):
         for orig_env_name, traj in demo_trajs_by_env.items()
     }
 
-    return demo_trajs_by_env, demo_env_names, variant_names
+    return demo_trajs_by_env, demo_env_names, variant_names, new_env_names
 
 
-def insert_task_ids(obs_seq, task_id, variant_id):
+def insert_task_source_ids(obs_seq, task_id, variant_id, source_id):
     nobs = len(obs_seq.obs)
     task_id_array = np.full((nobs, ), task_id, dtype='int64')
     variant_id_array = np.full((nobs, ), variant_id, dtype='int64')
+    source_id_array = np.full((nobs, ), source_id, dtype='int64')
     eio_arr = EnvIDObsArray(observation=obs_seq.obs,
                             task_id=task_id_array,
-                            variant_id=variant_id_array)
+                            variant_id=variant_id_array,
+                            source_id=source_id_array)
     return obs_seq._replace(obs=eio_arr)
 
 
@@ -201,16 +203,24 @@ def load_demos_mt(demo_paths,
                   omit_noop=False):
     # load demo pickles from disk and sort into dict
     demo_dicts = load_demos(demo_paths)
+    num_demo_sources = len(demo_dicts)
     demo_trajs_by_env = {}
-    for demo_dict in demo_dicts:
-        demo_trajs_by_env.setdefault(demo_dict['env_name'], []) \
-                         .append(demo_dict['trajectory'])
+    source_ids_by_env = {}
+    for source_id, demo_dict in enumerate(demo_dicts, start=1):
+        env_name = demo_dict['env_name']
+        traj = demo_dict['trajectory']
+        demo_trajs_by_env.setdefault(env_name, []).append(traj)
+        source_ids_by_env.setdefault(env_name, []).append(source_id)
 
     # if necessary, update the environment names to also include a preprocessor
     # name
     if mb_preproc:
-        demo_trajs_by_env, demo_env_names, variant_names \
+        demo_trajs_by_env, demo_env_names, variant_names, new_env_names \
             = add_mb_preproc(demo_trajs_by_env, variant_names, mb_preproc)
+        source_ids_by_env = {
+            new_env_names[k]: v
+            for k, v in source_ids_by_env.items()
+        }
     else:
         demo_env_names = sorted(set(demo_trajs_by_env))
 
@@ -218,9 +228,12 @@ def load_demos_mt(demo_paths,
     variant_groups = GroupedVariantNames(demo_env_names, variant_names)
     for env_name in demo_trajs_by_env.keys():
         task_id, variant_id = variant_groups.task_variant_by_name[env_name]
+        trajs = demo_trajs_by_env[env_name]
+        source_ids = source_ids_by_env[env_name]
+        assert len(trajs) == len(source_ids)
         demo_trajs_by_env[env_name] = [
-            insert_task_ids(traj, task_id, variant_id)
-            for traj in demo_trajs_by_env[env_name]
+            insert_task_source_ids(traj, task_id, variant_id, source_id)
+            for traj, source_id in zip(trajs, source_ids)
         ]
 
     # Convert the loaded trajectories into a torch Dataset. This removes
@@ -229,7 +242,7 @@ def load_demos_mt(demo_paths,
     dataset_mt = make_tensor_dict_dataset(demo_trajs_by_env,
                                           omit_noop=omit_noop)
 
-    return dataset_mt, variant_groups
+    return dataset_mt, variant_groups, num_demo_sources
 
 
 def make_loader_mt(dataset, batch_size):
@@ -244,7 +257,8 @@ def make_loader_mt(dataset, batch_size):
     # all tasks must be present for this to work
     assert torch.all(unique_ids == torch.arange(len(unique_ids))), (unique_ids)
     freqs_total = torch.sum(frequencies).to(torch.float)
-    unique_weights = frequencies.to(torch.float) / freqs_total
+    unique_weights = freqs_total / frequencies.to(torch.float)
+    unique_weights = unique_weights / unique_weights.sum()
     weights = unique_weights[task_ids]
 
     weighted_sampler = data.WeightedRandomSampler(weights,
@@ -272,10 +286,11 @@ def get_demos_meta(*,
                    omit_noop=False,
                    transfer_variants=(),
                    preproc_name=None):
-    dataset_mt, variant_groups = load_demos_mt(demo_paths,
-                                               transfer_variants,
-                                               mb_preproc=preproc_name,
-                                               omit_noop=omit_noop)
+    dataset_mt, variant_groups, num_demo_sources = load_demos_mt(
+        demo_paths,
+        transfer_variants,
+        mb_preproc=preproc_name,
+        omit_noop=omit_noop)
 
     print("Getting env metadata")
     all_env_names = sorted(variant_groups.task_variant_by_name.keys())
@@ -294,5 +309,6 @@ def get_demos_meta(*,
         'variant_groups': variant_groups,
         'env_metas': env_metas,
         'task_ids_and_demo_env_names': task_ids_and_demo_env_names,
+        'num_demo_sources': num_demo_sources,
     }
     return rv

mtil/sample_mux.py
@@ -172,21 +172,31 @@ class MuxGpuSampler(MuxParallelSampler, GpuSampler):
     pass
 
 
-EnvIDObs = NamedTupleSchema('EnvIDObs',
-                            ('observation', 'task_id', 'variant_id'))
+EnvIDObs = NamedTupleSchema(
+    'EnvIDObs',
+    # Conventions:
+    # - variant_id is 0 for the demonstration variant
+    # - source_id is 0 for novice rollouts, and can take values 1,2,… for all
+    #   others
+    ('observation', 'task_id', 'variant_id', 'source_id'))
 EnvIDObsArray = NamedArrayTupleSchema(EnvIDObs._typename, EnvIDObs._fields)
 
 
 class EnvIDWrapper(Wrapper):
-    def __init__(self, env, task_id, variant_id, num_tasks, max_num_variants):
+    def __init__(self, env, task_id, variant_id, source_id, num_tasks,
+                 max_num_variants, num_demo_sources):
         super().__init__(env)
         assert isinstance(task_id, int) and isinstance(variant_id, int)
         self._task_id_np = np.asarray([task_id]).reshape(())
         self._variant_id_np = np.asarray([variant_id]).reshape(())
+        self._source_id = np.asarray([source_id]).reshape(())
         task_space = IntBox(0, num_tasks)
         variant_space = IntBox(0, max_num_variants)
+        # remember, 0 is the novice!
+        source_space = IntBox(0, num_demo_sources + 1)
         self.observation_space = Composite(
-            (env.observation_space, task_space, variant_space), EnvIDObs)
+            (env.observation_space, task_space, variant_space, source_space),
+            EnvIDObs)
 
     def reset(self, *args, **kwargs):
         obs = super().reset(*args, **kwargs)
@@ -198,7 +208,8 @@ class EnvIDWrapper(Wrapper):
         return env_step._replace(observation=new_obs)
 
     def wrap_obs(self, obs):
-        new_obs = EnvIDObs._make((obs, self._task_id_np, self._variant_id_np))
+        new_obs = EnvIDObs._make(
+            (obs, self._task_id_np, self._variant_id_np, self._source_id))
         return new_obs
 
     @property
@@ -212,13 +223,20 @@ class MILBenchEnvMultiplexer:
     environments based on a *task ID* and *variant ID*. Useful for passing to
     rlpyt sampler constructors in order to get a different environment in each
     sampler process."""
-    def __init__(self, variant_groups):
+    def __init__(self, variant_groups, num_demo_sources=0):
         """Construct the multiplexer.
 
         Arguments:
             variant_groups (GroupdVariantNames): structure holding all of the
-                known tasks and task variant names. Contains task IDs too."""
+                known tasks and task variant names. Contains task IDs too.
+            num_demo_sources (int): the total number of demonstration source
+                IDs for whatever dataset is being used to train this algorithm.
+                Source ID 0 (special ID for novice rollouts) will be used for
+                all rollouts from this environment. This is only important when
+                using, e.g., Min-BC; if not using source data, then it is safe
+                to set it to 0."""
         self.variant_groups = variant_groups
+        self.num_demo_sources = num_demo_sources
 
     def get_batch_size_and_kwargs(self, min_batch_size):
         """Compute an sampler batch size that is >= min_batch_size
@@ -246,9 +264,14 @@ class MILBenchEnvMultiplexer:
         task_id, variant_id = env_key
         env_name = self.variant_groups.env_name_by_task_variant[env_key]
         mb_env = MILBenchGymEnv(env_name)
-        id_env = EnvIDWrapper(mb_env, task_id, variant_id,
-                              self.variant_groups.num_tasks,
-                              self.variant_groups.max_num_variants)
+        id_env = EnvIDWrapper(
+            mb_env,
+            task_id=task_id,
+            variant_id=variant_id,
+            source_id=0,  # novice
+            num_tasks=self.variant_groups.num_tasks,
+            max_num_variants=self.variant_groups.max_num_variants,
+            num_demo_sources=self.num_demo_sources)
         return id_env
 
 
@@ -273,10 +296,11 @@ class MuxTaskModelWrapper(AgentModelWrapper):
         return pi, v
 
 
-def make_mux_sampler(*, variant_groups, env_metas, use_gpu, batch_B, batch_T):
+def make_mux_sampler(*, variant_groups, num_demo_sources, env_metas, use_gpu,
+                     batch_B, batch_T):
     print("Setting up environment multiplexer")
     # local copy of Gym env, w/ args to create equivalent env in the sampler
-    env_mux = MILBenchEnvMultiplexer(variant_groups)
+    env_mux = MILBenchEnvMultiplexer(variant_groups, num_demo_sources)
     new_batch_B, env_ctor_kwargs = env_mux.get_batch_size_and_kwargs(batch_B)
     all_env_names = sorted(variant_groups.task_variant_by_name.keys())
     if new_batch_B != batch_B: