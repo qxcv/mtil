a751a9f8673f8fe8649e9b12683abf429b48cfa0
Sam Toyer <sam@qxcv.net>
2020-06-01 05:44:25 +0000

    Actually, on second thought, keep separate X servers

--- etc/comparison_runs.py
+++ etc/comparison_runs.py
@@ -189,7 +189,7 @@ def gen_command_gail(*,
                      **kwargs):
     extras = parse_unknown_args(**kwargs)
     cmd_parts = [
-        *("python -m mtil.algos.mtgail").split(),
+        *("xvfb-run -a python -m mtil.algos.mtgail").split(),
         "--out-dir",
         out_root,
         "--run-name",
@@ -232,6 +232,8 @@ def gen_command_bc(*,
         f"transfer envs not yet supported for BC, but got transfer env " \
         f"names '{trans_env_names}'"
     cmd_parts = [
+        "xvfb-run",
+        "-a",
         "python",
         "-m",
         "mtil.algos.mtbc",
